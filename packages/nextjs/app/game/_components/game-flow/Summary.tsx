dd72210137b6518b79ae03620af28e817a3a05b9
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2024-10-05 17:39:00 +0800

    back to home after end

--- packages/nextjs/app/game/_components/game-flow/Summary.tsx
+++ packages/nextjs/app/game/_components/game-flow/Summary.tsx
@@ -2,22 +2,26 @@ import React from "react";
 import { Button } from "../Button";
 import { Card } from "../Card";
 import { motion } from "framer-motion";
+import { useGlobalState } from "~~/services/store/store";
 
 const story =
   'In the quaint downtown cafe, lined with books and the soft hum of indie music, Ellie bumped into Alex, her old high school crush, purely by chance. The surprise was evident on both faces.\n\n"Alex? Wow, this is unexpected!" Ellie\'s heart raced as she balanced her tray.\n\n"Ellie, right? I can\'t believe it\'s been so long!" Alex smiled, looking genuinely pleased. They found a table near the window, the rain outside casting a cozy glow inside.\n\nAs they sipped their lattes, the conversation flowed easily, touching on past memories and current dreams. The spark between them was undeniable.\n\n"Listen, I know this is sudden, but would you like to go out sometime? Maybe dinner?" Alex asked, hopeful.\n\nEllie\'s smile said it all. "I\'d love to, Alex. It\'s like something out of a storybook, meeting like this."\n\nThey exchanged numbers, the promise of a new beginning just a phone call away, leaving the cafe with more warmth in their hearts than the coffee could ever offer.';
 
 type Direction = "left" | "right";
 export const Summary = () => {
+  const [setPage] = useGlobalState(state => [state.setPage]);
   const onSwiped = (direction: Direction) => {
     console.log(`Swiped ${direction}`);
   };
 
   const onLike = () => {
     console.log("Liked", { story });
+    setPage("home");
   };
 
   const onPass = () => {
     console.log("Passed", { story });
+    setPage("home");
   };
 
   return (
